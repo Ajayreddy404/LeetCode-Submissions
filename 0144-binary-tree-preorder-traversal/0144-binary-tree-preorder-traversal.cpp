de64637c2f9cb643ed921e23f6185de813d8b327
Ajay Reddy <87086062+Ajayreddy404@users.noreply.github.com>
2025-01-03 21:57:40 +0530

    Time: 0 ms (100%), Space: 10.9 MB (32.49%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -20,8 +20,35 @@ public:
     }
 
     vector<int> preorderTraversal(TreeNode* root) {
-        vector<int> ans;
-        dfs(root, ans);
-        return ans;
+        // vector<int> ans;
+        // dfs(root, ans);
+        // return ans;
+
+        // Morris traversal
+        TreeNode* node = root;
+        vector<int> arr;
+        while(node!=NULL){
+            arr.push_back(node->val);
+            if(node->left==NULL){
+                node = node->right;
+                
+            }else{
+                TreeNode* nd = node->left;
+                while(nd->right!=NULL && nd->right!=node){
+                    nd = nd->right;
+                }
+
+                if(nd->right==NULL){
+                    nd->right = node;
+                    node = node->left;
+                }else{
+                    nd->right = NULL;
+                    node = node->right;
+                    // prev = true;
+                    arr.pop_back();
+                }
+            }
+        }
+        return arr;
     }
 };
