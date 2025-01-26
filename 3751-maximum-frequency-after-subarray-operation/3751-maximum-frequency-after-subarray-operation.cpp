a96b3ebd6460706c2c9f9fa60ed2ec2e2e5c5d88
Ajay Reddy <87086062+Ajayreddy404@users.noreply.github.com>
2025-01-26 14:19:27 +0530

    Time: 49 ms (72.22%), Space: 96.4 MB (50%) - LeetHub

--- 3751-maximum-frequency-after-subarray-operation/3751-maximum-frequency-after-subarray-operation.cpp
+++ 3751-maximum-frequency-after-subarray-operation/3751-maximum-frequency-after-subarray-operation.cpp
@@ -6,12 +6,11 @@ public:
             if(x==k)    kf++;
             max_x = max(max_x, x);
         }
-        // cout<<kf<<endl;
-        int ans = 0, ckmm = 0;
+        
+        int ans = 0;
         for(int x=1; x<=max_x; x++){
-            // cout<<"x: "<<x<<endl;
             if(x==k)    continue;
-            int fq = 0, max_fq = 0, ck = 0, ckm = 0;
+            int fq = 0, max_fq = 0;
             for(int i=0; i<nums.size(); i++){
                 
                 if(nums[i]==k){
@@ -22,19 +21,14 @@ public:
                 }
                 if(fq<=0){
                     fq = 0;
-                    ck = 0;
                 }
                 if(max_fq<fq){
                     max_fq = fq;
-                    ckm = ck;
                 }
-                // if(x==9)    cout<<i<<" "<<ck<<endl;
             }
             if(ans<max_fq){
                 ans = max_fq;
-                ckmm = ckm;
             }
-            // cout<<max_fq<<" "<<ckm<<endl;
         }
         return ans+kf;
     }
